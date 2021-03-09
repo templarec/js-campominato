f8d98c5e174f0f2529c8300a9e69acae5fd6d963
Lorenzo Bernini <templarec@gmail.com>
2021-03-09 17:50:34 +0100

    Individuazione micro-problemi

---
+++ main.js
@@ -0,0 +1,12 @@
+//genero 16 numeri da 1 a 100
+//li metto nell'array delle bombe
+//metto i restanti nell'array safe
+//chiedo 100 - 16 volte all'utente di inserire un numero
+	//controllo che non mi metta più volte lo stesso numero
+//controllo che ogni numero non sia nell'array bombe
+	//conto quanti numeri del safe array becca e lo stampo quando:
+		//becca una bomba
+		//finisce array safe
+
+//BONUS:
+//funzionalità difficoltà
